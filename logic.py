f1c6aa3c87bdc244203f7e7767a34f88dec75339
megha shastry <meghashastry06@gmail.com>
2023-12-12 21:23:02 +0100

    Add 2 in two random places to begin the game

--- logic.py
+++ logic.py
@@ -1,24 +1,62 @@
 
 import random
 
-def start_game():
-    # Assuming GUI to be 4*4 matrix:
-    #initialize empty list. This list holds [[2], [0], [4],[8]] 
-    # i points row count. 
-    empty_list = []
-    for i in range(4):
-        empty_list.append([0] * 4)
+def start_game(n):
+    #Create Matrix
+    matrix = []
+    for i in range(n):
+        matrix.append([0] * n)
 
-    # Print controls
-    print("Commands are as follows : ")
-    print("'W' or 'w' : Move Up")
-    print("'S' or 's' : Move Down")
-    print("'A' or 'a' : Move Left")
-    print("'D' or 'd' : Move Right")
+    #To Begin game add "2" in two random places
+    matrix = add_2(matrix)
+    matrix = add_2(matrix)
 
-    # Random cells will be filled with number 2
-    add_list(empty_list)
-    return empty_list
+    ##Test
+    # for i in matrix:
+    #     print(i)
 
-def add_list(add_2_to_grid):
-    add_2_to_grid = random.randint()
+    return matrix
+
+def add_2(matrix) :
+    ## Original code
+    # #Select random cell to place "2"
+    row = random.randint(0, len(matrix)-1)
+    coloumn = random.randint(0, len(matrix)-1)
+
+    #Checking for availability. If the chosen cell is not empty, select random cell to place "2"
+    while matrix[row][coloumn] != 0:
+        row = random.randint(0, len(matrix)-1)
+        coloumn = random.randint(0, len(matrix)-1)
+
+    #Place "2" in the selected cell    
+    matrix[row][coloumn] = 2
+
+    ##Test
+    #matrix[0][3] = 2048
+
+    return matrix
+
+def game_state(matrix):
+    #Winning Condition
+    for i in range(len(matrix)):
+        #Test
+        #print("i", matrix[i])
+        for j in range(len(matrix)):
+            #Test
+            #print("j", matrix[j])
+            if matrix[i][j] == 2048:
+                return 'Win!'
+        
+        #Checking game not over condition. i.e Checking for any zero entries
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if matrix[i][j] == 0:
+                    return 'Not Over'
+        
+        
+
+            
+
+
+start_game(4)
+    
