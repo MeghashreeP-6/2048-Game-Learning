9beacd9e06e22478a415c4568071b8cba22cf349
Meghashree Prakash <meghashastry06@gmail.com>
2024-01-13 12:06:22 +0100

    Add Grid functionalities

--- puzzle.py
+++ puzzle.py
@@ -1,17 +1,30 @@
-from tkinter import Frame, Label, CENTER
 import random
+#Frame:
+    #Python Tkinter Frame widget is used to organize the group of widgets. 
+    #It acts like a container which can be used to hold the other widgets. 
+#Label:
+    #This widget implements a display box where you can place text or images.
+#Center:
+    #The default for a ttk label is to be aligned left, but the tkinter label is aligned center, 
+    #and the order of your imports means that you're using a ttk Label. 
+    #The quick fix for your example is to explicitly set the anchor option for the ttk label 
+    #(eg: label. configure(anchor="center") ).
+from tkinter import Frame, Label, CENTER
 import logic
-import constants as c
+import constants as c 
 
-def gen():
-    return random.randint(0, c.GRID_LEN - 1)
+# def gen():
+#     return random.randint(0,c.GRID_LEN -1)
 
-class GameGrid(Frame):
+#Initialise a frame
+class GridGame(Frame):
     def __init__(self):
         Frame.__init__(self)
 
+        #Grid method is used to create grid
         self.grid()
         self.master.title('2048')
+        #bind() method of Tkinter is utilized to connect an event passed in the widget along with the event handler.
         self.master.bind("<Key>", self.key_down)
 
         self.commands = {
@@ -26,20 +39,22 @@ class GameGrid(Frame):
             c.KEY_UP_ALT2: logic.move_up,
             c.KEY_DOWN_ALT2: logic.move_down,
             c.KEY_LEFT_ALT2: logic.move_left,
-            c.KEY_RIGHT_ALT2: logic.move_right,
+            c.KEY_RIGHT_ALT2: logic.move_right
+            
         }
 
         self.grid_cells = []
         self.init_grid()
         self.matrix = logic.start_game(c.GRID_LEN)
-        #self.matrix = logic.start_game(c.GRID_LEN)
         self.history_matrixs = []
         self.update_grid_cells()
-
+        #mainloop() is used when your application is ready to run. mainloop() is an infinite loop used to run the application,
+        #wait for an event to occur and process the event as long as the window is not closed.
         self.mainloop()
 
+    #Initialize grid with all the settings. i.e cell and grid colour are filled
     def init_grid(self):
-        background = Frame(self,width=c.SIZE, height=c.SIZE)
+        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,width=c.SIZE, height=c.SIZE)
         background.grid()
 
         for i in range(c.GRID_LEN):
@@ -68,38 +83,38 @@ class GameGrid(Frame):
                 t.grid()
                 grid_row.append(t)
             self.grid_cells.append(grid_row)
-        #self.pack()
-        #self.mainloop()
 
+    #Update grid: Change cell colour according to the number present in the grid.
     def update_grid_cells(self):
         for i in range(c.GRID_LEN):
             for j in range(c.GRID_LEN):
                 new_number = self.matrix[i][j]
-                #print(new_number)
                 if new_number == 0:
                     self.grid_cells[i][j].configure(text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    #self.grid_cells[i][j].configure(text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
                 else:
                     self.grid_cells[i][j].configure(
                         text=str(new_number),
                         bg=c.BACKGROUND_COLOR_DICT[new_number],
                         fg=c.CELL_COLOR_DICT[new_number]
+        
                     )
+        #Calls all pending idle tasks, without processing any other events
         self.update_idletasks()
 
     def key_down(self, event):
+        #keysyms - keysyms recognized by Tk.A single-character string that is the key's code (only for keyboard events) keysym.
+        # A string that is the key's symbolic name (only for keyboard events)
         key = event.keysym
-        print("Key_Down")
         print(event)
         if key == c.KEY_QUIT: exit()
         if key == c.KEY_BACK and len(self.history_matrixs) > 1:
             self.matrix = self.history_matrixs.pop()
             self.update_grid_cells()
-            #print('back on step total step:', len(self.history_matrixs))
+            print('total step:', len(self.history_matrixs))
         elif key in self.commands:
             self.matrix, done = self.commands[key](self.matrix)
             if done:
-                self.matrix = logic.add_2(self.matrix)
+                self.matrix = logic.add_two(self.matrix)
                 # record last move
                 self.history_matrixs.append(self.matrix)
                 self.update_grid_cells()
@@ -110,4 +125,10 @@ class GameGrid(Frame):
                     self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
                     self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
 
-game_grid = GameGrid()
+    # def generate_next(self):
+    #     index = (gen(), gen())
+    #     while self.matrix[index[0]][index[1]] != 0:
+    #         index = (gen(), gen())
+    #     self.matrix[index[0]][index[1]] = 2
+
+game_grid = GridGame()
